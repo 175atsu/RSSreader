2cf2381f178836252794b3da3b60eb4d150ee0fb
Atsu <zatsuyou0@gmail.com>
2018-10-01 10:00:22 +0900

    クラス整理

--- RSSreader/ViewController.swift
+++ RSSreader/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     let feedUrl = URL(string: "https://news.yahoo.co.jp/pickup/rss.xml")!
     var feedItems = [FeedItem]()
     
+    var parser = [Parser]()
+    
     var currentElementName : String! // RSSパース中の現在の要素名
     
     let ITEM_ELEMENT_NAME = "item"
@@ -41,44 +43,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     
-    let parser = ParserController()
-    
-    
-    // 解析中に要素の開始タグがあったときに実行されるメソッド
-    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
-        self.currentElementName = nil
-        if elementName == ITEM_ELEMENT_NAME {
-            self.feedItems.append(FeedItem())
-        } else {
-            currentElementName = elementName
-        }
-    }
-    
-    // 開始タグと終了タグでくくられたデータがあったときに実行されるメソッド
-    func parser(_ parser: XMLParser, foundCharacters string: String) {
-        if self.feedItems.count > 0 {
-            let lastItem = self.feedItems[self.feedItems.count - 1]
-            switch self.currentElementName {
-            case TITLE_ELEMENT_NAME:
-                let tmpString = lastItem.title
-                lastItem.title = (tmpString != nil) ? tmpString! + string : string
-            case LINK_ELEMENT_NAME:
-                lastItem.url = string
-            default: break
-            }
-        }
-    }
-    
-    // 解析中に要素の終了タグがあったときに実行されるメソッド
-    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-        self.currentElementName = nil
-    }
-    
-    // XML解析終了時に実行されるメソッド
-    func parserDidEndDocument(_ parser: XMLParser) {
-        self.tableView.reloadData()
-    }
-    
     // 初期表示時に必要な処理を設定します。
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -95,7 +59,51 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 }
 
+var viewcontroller = ViewController()
+
+
 class FeedItem {
     var title: String!
     var url: String!
+}
+
+
+
+class Parser: UIViewController, XMLParserDelegate {
+    
+    
+    // 解析中に要素の開始タグがあったときに実行されるメソッド
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        viewcontroller.currentElementName = nil
+        if elementName == viewcontroller.ITEM_ELEMENT_NAME {
+            viewcontroller.feedItems.append(FeedItem())
+        } else {
+            viewcontroller.currentElementName = elementName
+        }
+    }
+    
+    // 開始タグと終了タグでくくられたデータがあったときに実行されるメソッド
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if viewcontroller.feedItems.count > 0 {
+            let lastItem = viewcontroller.feedItems[viewcontroller.feedItems.count - 1]
+            switch viewcontroller.currentElementName {
+            case viewcontroller.TITLE_ELEMENT_NAME:
+                let tmpString = lastItem.title
+                lastItem.title = (tmpString != nil) ? tmpString! + string : string
+            case viewcontroller.LINK_ELEMENT_NAME:
+                lastItem.url = string
+            default: break
+            }
+        }
+    }
+    
+    // 解析中に要素の終了タグがあったときに実行されるメソッド
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        viewcontroller.currentElementName = nil
+    }
+    
+    // XML解析終了時に実行されるメソッド
+    func parserDidEndDocument(_ parser: XMLParser) {
+        viewcontroller.tableView.reloadData()
+    }
 }
